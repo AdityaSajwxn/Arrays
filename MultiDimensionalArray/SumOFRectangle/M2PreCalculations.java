b54f02f27a389d4447ec8d067db273185ebfa3a7
Aditya Sajwan <sajwan.aditya@outlook.com>
2025-02-19 23:33:24 +0530

    Mehthod 2 for Sum of Rectangle

--- MultiDimensionalArray/SumOFRectangle/M2PreCalculations.java
+++ MultiDimensionalArray/SumOFRectangle/M2PreCalculations.java
@@ -12,15 +12,7 @@ class RequiredFunctions{
     }
     } 
 
-    static void PrintRectangle(int[][] arr, int l1, int r1, int l2, int r2) {
-        System.out.println("The rectangle elements are:");
-        for (int i = l1; i <= l2; i++) {
-            for (int j = r1; j <= r2; j++) {
-                System.out.print(arr[i][j] + " ");
-            }
-            System.out.println(); // Move to the next row after printing a row of the rectangle
-        }
-    }
+   
 }
 
 
