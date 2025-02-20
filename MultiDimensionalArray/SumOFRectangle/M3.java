07b849ea4d48ef4d8f83ac769da1b1e27f02ec59
Aditya Sajwan <sajwan.aditya@outlook.com>
2025-02-20 22:58:03 +0530

    test commit

---
+++ MultiDimensionalArray/SumOFRectangle/M3.java
@@ -0,0 +1,31 @@
+package MultiDimensionalArray.SumOFRectangle;
+import java.util.Scanner;
+
+class RequiredFunctions4{
+
+    static void print2DArray(int[][]arr){
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[i].length;j++){
+                System.out.print(arr[i][j] +  "  ");
+            }
+            System.out.println();
+    }
+    }     
+}
+
+
+public class M3 {
+     
+
+
+
+    public static void main(System[]args){
+        Scanner sc = new Scanner(System.in);
+
+
+        sc.close();
+    }
+
+    
+    
+}
